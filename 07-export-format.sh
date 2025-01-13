92d3bf8517e97a82dda102bc37f9f85b6d097498
Natalie Forsdick <20059249+natforsdick@users.noreply.github.com>
2025-01-13 14:48:58 +1300

    /path/to/ syntax

--- 07-export-format.sh
+++ 07-export-format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 ml PLINK/1.09b6.16
-INDIR=/nesi/nobackup/ga03048/Weta_GBS_Batch2/03_ref_map_PE/all2/
+INDIR=/path/to/03_ref_map_PE/all2/
 VCF=weta.snpmiss60.mac3.thin
 plink --vcf ${INDIR}${VCF}.vcf --make-bed --aec --out ${INDIR}${VCF}
 head ${INDIR}${VCF}.bim
